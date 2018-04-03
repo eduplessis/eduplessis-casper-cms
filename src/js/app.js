ece3ef3baa405bb6983221ba2377683afc502c71
Edouard Duplessis <edouard@motionindesign.com>
2018-04-03 10:37:40 -0400

    first test cms

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,5 @@
-alert('test');
-
-
-
-
 if (window.netlifyIdentity) {
-  window.netlifyIdentity.on("init", user => {
+  window.netlifyIdentity.on("init", (user) => {
     if (!user) {
       window.netlifyIdentity.on("login", () => {
         document.location.href = "/admin/";
