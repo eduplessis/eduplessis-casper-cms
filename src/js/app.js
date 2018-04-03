8fafb2ff4232046b690f4d38f8a601623911b35a
Edouard Duplessis <eduplessis@gmail.com>
2018-04-02 23:12:04 -0400

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,11 @@
 // JS Goes here - ES6 supported
+
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", user => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
+  });
+}
